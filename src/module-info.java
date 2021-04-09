f560d0092f967955dc7baf9c47f2ce1547a9991e
Emmaunuel Edorh <emmanoedorh@gmail.com>
2021-04-08 19:41:04 -0500

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module EmailClient {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires javafx.swt;
+
+    opens com.emmano.emailclient;
+}
